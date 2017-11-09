8c97ca263f2564551aaec059962d6ed15d8aa5d4
Stas <arhmachael@gmail.com>
2017-11-09 23:12:22 +0200

    Add files via upload

---
+++ SearchStuff/Serch/Поиск/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace SearchThis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(249, 465);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox1.Location = new System.Drawing.Point(527, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(202, 465);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 327);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(183, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(7, 368);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(183, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Поиск";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 235);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(183, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 276);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(183, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Открыть ссылку";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(4, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(192, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Открыть файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 28);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 63);
+            this.button1.Margin = new System.Windows.Forms.Padding(1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 21);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Поиск слова";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(249, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(278, 465);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 465);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "Form1";
+            this.Text = "Поиск по тексту";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ SearchStuff/Serch/Поиск/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mshtml;
+
+
+namespace SearchThis
+{
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = 0;
+            richTextBox1.SelectionLength = richTextBox1.Text.Length;
+            richTextBox1.SelectionColor = Color.Black;
+            string str = textBox1.Text;
+            int i = 0;
+            while (i <= richTextBox1.Text.Length - str.Length)
+            {
+                i = richTextBox1.Text.IndexOf(str, i);
+                if (i < 0) break;
+                richTextBox1.SelectionStart = i;
+                richTextBox1.SelectionLength = str.Length;
+                richTextBox1.SelectionColor = Color.Red;
+                i += str.Length;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text files | *.txt";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                richTextBox1.LoadFile(dialog.FileName, RichTextBoxStreamType.PlainText);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = 0;
+            richTextBox1.SelectionLength = richTextBox1.Text.Length;
+            richTextBox1.SelectionColor = Color.Black;
+            textBox1.Text = "";
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text files | *.txt";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                richTextBox1.LoadFile(dialog.FileName, RichTextBoxStreamType.PlainText);
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = 0;
+            richTextBox1.SelectionLength = richTextBox1.Text.Length;
+            richTextBox1.SelectionColor = Color.Black;
+            textBox1.Text = "";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = 0;
+            richTextBox1.SelectionLength = richTextBox1.Text.Length;
+            richTextBox1.SelectionColor = Color.Black;
+            string str = textBox1.Text;
+            int i = 0;
+            while (i <= richTextBox1.Text.Length - str.Length)
+            {
+                i = richTextBox1.Text.IndexOf(str, i);
+                if (i < 0) break;
+                richTextBox1.SelectionStart = i;
+                richTextBox1.SelectionLength = str.Length;
+                richTextBox1.SelectionColor = Color.Red;
+                i += str.Length;
+            }
+        }
+
+
+        public static void Find(WebBrowser webBrowser, string searchString)
+        {
+            // you can add check page load complette, if needed
+            IHTMLDocument2 document = webBrowser.Document?.DomDocument as IHTMLDocument2;
+            IHTMLTxtRange range = document?.selection.createRange() as IHTMLTxtRange;
+            if (range == null) return;
+
+            if (!range.findText(searchString)) return;
+
+            // select
+            range.@select();
+            // make highlighted
+            range.pasteHTML("<span id='i-am-selected' style='background-color: rgb(255, 255, 0);'>"
+                             + searchString + "</span>");
+
+            // and i want see find result on top of page
+            // 1. scroll to page end
+            webBrowser.Document.Body?.ScrollIntoView(false);
+            // 2. scroll to find result
+            range.scrollIntoView();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Find(webBrowser1, textBox3.Text);
+            
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://" + textBox2.Text);
+        }
+    }
+    }
+
